cac439ed76937e57079c5679a73023bd241f991f
Mira Feiring <mirafeiring@gmail.com>
2023-02-16 11:24:09 +0100

    Add more symlinks

--- setup/init.sh
+++ setup/init.sh
@@ -3,12 +3,6 @@
 # Copy fonts
 cp -a fonts/ ~/Library/Fonts/
 
-# Symlinks
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/development/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/development/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/development/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
-
 # This installs homebrew itself, and also the command line tools in silent mode
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -21,3 +15,10 @@ mkdir ~/.nvm
 mkdir ~/development
 export STARSHIP_CONFIG=~/dotfiles/config/starship.toml
 source ~/dotfiles/.zshrc
+
+# Symlinks
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+ln -s ~/dotfiles/.hyper.js ~/.hyper.js
